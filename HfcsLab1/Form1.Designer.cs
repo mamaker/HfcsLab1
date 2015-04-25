976d86954129b89c96cdee12180880d28afec2e6
mamaker <madhuvasudevan@hotmail.com>
2015-04-25 00:25:12 -0500

    Pause 1

--- HfcsLab1/Form1.Designer.cs
+++ HfcsLab1/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.raceTrackPictureBox = new System.Windows.Forms.PictureBox();
+            this.Greyhound1 = new System.Windows.Forms.PictureBox();
+            this.Greyhound2 = new System.Windows.Forms.PictureBox();
+            this.Greyhound3 = new System.Windows.Forms.PictureBox();
+            this.Greyhound4 = new System.Windows.Forms.PictureBox();
+            this.bettingParlour = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.raceTrackPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // raceTrackPictureBox
+            // 
+            this.raceTrackPictureBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.raceTrackPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("raceTrackPictureBox.Image")));
+            this.raceTrackPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.raceTrackPictureBox.Name = "raceTrackPictureBox";
+            this.raceTrackPictureBox.Size = new System.Drawing.Size(1303, 375);
+            this.raceTrackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.raceTrackPictureBox.TabIndex = 0;
+            this.raceTrackPictureBox.TabStop = false;
+            // 
+            // Greyhound1
+            // 
+            this.Greyhound1.Image = ((System.Drawing.Image)(resources.GetObject("Greyhound1.Image")));
+            this.Greyhound1.Location = new System.Drawing.Point(13, 13);
+            this.Greyhound1.Name = "Greyhound1";
+            this.Greyhound1.Size = new System.Drawing.Size(137, 50);
+            this.Greyhound1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Greyhound1.TabIndex = 1;
+            this.Greyhound1.TabStop = false;
+            // 
+            // Greyhound2
+            // 
+            this.Greyhound2.Image = ((System.Drawing.Image)(resources.GetObject("Greyhound2.Image")));
+            this.Greyhound2.Location = new System.Drawing.Point(13, 98);
+            this.Greyhound2.Name = "Greyhound2";
+            this.Greyhound2.Size = new System.Drawing.Size(137, 50);
+            this.Greyhound2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Greyhound2.TabIndex = 2;
+            this.Greyhound2.TabStop = false;
+            // 
+            // Greyhound3
+            // 
+            this.Greyhound3.Image = ((System.Drawing.Image)(resources.GetObject("Greyhound3.Image")));
+            this.Greyhound3.Location = new System.Drawing.Point(13, 198);
+            this.Greyhound3.Name = "Greyhound3";
+            this.Greyhound3.Size = new System.Drawing.Size(137, 50);
+            this.Greyhound3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Greyhound3.TabIndex = 3;
+            this.Greyhound3.TabStop = false;
+            // 
+            // Greyhound4
+            // 
+            this.Greyhound4.Image = ((System.Drawing.Image)(resources.GetObject("Greyhound4.Image")));
+            this.Greyhound4.Location = new System.Drawing.Point(13, 298);
+            this.Greyhound4.Name = "Greyhound4";
+            this.Greyhound4.Size = new System.Drawing.Size(137, 50);
+            this.Greyhound4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Greyhound4.TabIndex = 4;
+            this.Greyhound4.TabStop = false;
+            // 
+            // bettingParlour
+            // 
+            this.bettingParlour.Location = new System.Drawing.Point(0, 382);
+            this.bettingParlour.Name = "bettingParlour";
+            this.bettingParlour.Size = new System.Drawing.Size(1291, 169);
+            this.bettingParlour.TabIndex = 5;
+            this.bettingParlour.TabStop = false;
+            this.bettingParlour.Text = "Betting Parlor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1303, 582);
+            this.Controls.Add(this.bettingParlour);
+            this.Controls.Add(this.Greyhound4);
+            this.Controls.Add(this.Greyhound3);
+            this.Controls.Add(this.Greyhound2);
+            this.Controls.Add(this.Greyhound1);
+            this.Controls.Add(this.raceTrackPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Day At The Races";
+            ((System.ComponentModel.ISupportInitialize)(this.raceTrackPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Greyhound4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox raceTrackPictureBox;
+        private System.Windows.Forms.PictureBox Greyhound1;
+        private System.Windows.Forms.PictureBox Greyhound2;
+        private System.Windows.Forms.PictureBox Greyhound3;
+        private System.Windows.Forms.PictureBox Greyhound4;
+        private System.Windows.Forms.GroupBox bettingParlour;
     }
 }
 
